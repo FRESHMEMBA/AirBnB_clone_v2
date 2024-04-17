d67c735bdc4a8e3e2bdb8892510d76d81acfca49
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-04-17 22:58:45 +0200

    5. Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns the list of objects of one type of class"""
-        # return FileStorage.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
