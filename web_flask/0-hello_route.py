6235adcc2fd2d8c561771e3160d5f8886a21161a
Sibonelo Mazibuko <123705498+FRESHMEMBA@users.noreply.github.com>
2024-08-07 02:39:44 +0200

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,8 @@ Starts a Flask web application:
 Web application listens on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
-Uses the option strict_slashes=False in your route definition
+Uses the option strict_slashes=False in route definition
 """
-
 
 from flask import Flask
 
