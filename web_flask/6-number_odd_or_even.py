f3c027cef482b63b5109769cde5319652f39d455
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-09-09 16:34:45 +0200

    odd or even remove redundant is

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -56,7 +56,7 @@ def number_template(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    evenness = "is even" if n % 2 == 0 else "is odd"
+    evenness = "even" if n % 2 == 0 else "odd"
     return render_template('6-number_odd_or_even.html', n=n, evenness=evenness)
 
 if __name__ == "__main__":
