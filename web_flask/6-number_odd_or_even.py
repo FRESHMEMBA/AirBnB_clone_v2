1ff33a9e368fc66511a6079bcac75ffb096ac7c5
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-09-09 16:32:38 +0200

    odd or even render .html instead of .py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -57,7 +57,7 @@ def number_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     evenness = "is even" if n % 2 == 0 else "is odd"
-    return render_template('6-number_odd_or_even.py', n=n, evenness=evenness)
+    return render_template('6-number_odd_or_even.html', n=n, evenness=evenness)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
