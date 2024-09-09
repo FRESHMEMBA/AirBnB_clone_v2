0a3997e5dfeaf4e70a213d72fb0ab24bfef77ca8
FRESHMEMBA <smazibuko023@student.wethinkcode.co.za>
2024-09-09 14:58:53 +0200

    strict_slashes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -47,7 +47,7 @@ def number_route(n):
     return f"{n} is a number"
 
 
-@app.route('/number_template/<int:n>', stric_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     return render_template('5-number.html', number=n)
 
