6b930d99ab597b5c61406ff2aaa4ce9bf4865f33
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-09-09 12:08:53 +0200

    is it a number? without else

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -43,7 +43,8 @@ def python_text(text="is cool"):
 
 @app.route('/number/<n>', strict_slashes=False)
 def number_route(n):
-    return f"{n} is a number" if isinstance(n, int) else ""
+    if isinstance(n, int):
+        return f"{n} is a number"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
